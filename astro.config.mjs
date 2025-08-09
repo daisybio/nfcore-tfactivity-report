80b4fe55e039c79f70098433169b7be71241b8c6
nictru <nictru32@gmail.com>
2025-08-09 11:36:35 +0200

    Use astro-relative-links plugin

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
+import relativeLinks from "astro-relative-links";
+
 // https://astro.build/config
 export default defineConfig({
+  integrations: [relativeLinks()],
   vite: {
     plugins: [tailwindcss()],
   },
